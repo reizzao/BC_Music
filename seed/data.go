2ca5113d71c6e687e24976e8d5519432522a7e4d
@rzj <60945992+reizzao@users.noreply.github.com>
2025-04-13 16:25:29 -0300

    Atualizar o data.go

--- seed/data.go
+++ seed/data.go
@@ -1,1 +1,3 @@
 //Todo :  mudar essa pasta para data e nela add este data e os tipos que contem dados porque são dados 
+
+- refazer todas as repartiçoes de pastas, usar a técnica :: Poder ter mais agrupe tudo nele como um objeto .
