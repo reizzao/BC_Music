589f7d323409b9bb5356d29bb7d5311dfec11a50
reizzao <reizao1977@gmail.com>
2025-04-29 13:51:20 -0300

    feat: [guia-composicao] inicio

--- modulos/guia_composicao/main.go
+++ modulos/guia_composicao/main.go
@@ -1,11 +1,34 @@
 package guia_composicao
 
-import "github.com/musicalrzj/global"
-
 type Guia_Composicao struct {
-	Frase_Por_Cena                    Def_Frase_Por_Cena
-	Silabas_Poeticas_NotasPorCompasso Def_Silabas_Poeticas
+	Guia_Frase Guia_Frase
+	// Frase_Por_Cena                    Def_Frase_Por_Cena
+	// Silabas_Poeticas_NotasPorCompasso Def_Silabas_Poeticas
 }
 
-type Def_Frase_Por_Cena = global.Numeracao
-type Def_Silabas_Poeticas = global.Numeracao
+// type Def_Frase_Por_Cena = global.Numeracao
+// type Def_Silabas_Poeticas = global.Numeracao
+
+type Guia_Frase struct {
+	GPergunta PropsPartFrase
+	GResposta PropsPartFrase
+}
+
+type PropsPartFrase struct {
+	AberturaSom   OpAberturaSom
+	Interpretacao OpInterpretacao
+}
+
+type OpAberturaSom string
+
+const (
+	Fechado = "Fechado"
+	Aberto  = "Aberto"
+)
+
+type OpInterpretacao string
+
+const (
+	Perguntando = "Perguntando"
+	Explicando  = "Explicando"
+)
