011399a46a1ac05b47c3046205abb55171ae2d78
reizzao <reizao1977@gmail.com>
2025-05-08 12:44:22 -0300

    refactor: [todo] nova tipagem composicao

--- modulos/composicao/use_composicao/create.types.go
+++ modulos/composicao/use_composicao/create.types.go
@@ -1,31 +1,52 @@
 package use_composicao
 
-import (
-	"github.com/musicalrzj/modulos/composicao"
-)
-
-type Composicao struct {
-	Sinopse        Sinopse
-	Cenas_Estrofes Props_Cenas_Estrofes
-
+type ComposicaoMain struct {
+	PerguntasObrigatorias IPerguntasObrigatorias
+	Partes                IPartes
 }
 
-type Sinopse struct {
-	SentimentoCentral composicao.OpSentimento
+type IPerguntasObrigatorias struct {
+	Tema                       string
+	Sentimento_Central_do_Tema string
+	Sentimento_Master          string
 }
 
-type Frase struct {
-	Pergunta string
-	Resposta string
+type IPartes struct {
+	Parte_A        PartesModel
+	Parte_B        PartesModel
+	Parte_R_01_IN  PartesModel
+	Parte_R_01_OUT PartesModel
+	Parte_C        PartesModel
 }
 
-type Props_Cenas_Estrofes struct {
-	A_Apresentacao Frase
-	A_Resolucao    Frase
-	B_Apresentacao Frase
-	B_Resolucao    Frase
-	R_Apresentacao Frase
-	R_Resolucao    Frase
-	E_Apresentacao Frase
-	E_Resolucao    Frase
+type PartesModel struct {
+	Fixo    PartesFixo
+	Request PartesRequest
+}
+
+type PartesFixo struct {
+	Identificador      string
+	MissaoDaParte      string
+	TecnicaDaParte     string
+	Metrica            string
+	VelocidadeMelodica string
+	VocalSom           string
+	Exemplos           []string
+	Tutoriais          []string
+}
+
+type PartesRequest struct {
+	Frases IFrases
+}
+
+type IFrases struct {
+	Frase_01_Apresenta IFrasesModel
+	Frase_02_Conclui   IFrasesModel
+}
+
+type IFrasesModel struct {
+	MissaoFrase string
+	Inicio      string
+	Meio        string
+	Fim         string
 }
